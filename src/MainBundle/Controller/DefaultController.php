ce808314bd88e4c9677ff581a0f64e15268c6515
davidcopano <davidcopano96@gmail.com>
2018-06-13 18:02:01 +0200

    Añadido bundle

---
+++ src/MainBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/main")
+     */
+    public function indexAction()
+    {
+        return $this->render('@Main/Default/index.html.twig');
+    }
+}
