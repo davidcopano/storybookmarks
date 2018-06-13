d41be1bc8f7baff38266ced527c066c4a1910709
davidcopano <davidcopano96@gmail.com>
2018-06-13 18:22:49 +0200

    Añadido FOSUserBundle

---
+++ src/MainBundle/Entity/User.php
@@ -0,0 +1,61 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace MainBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
+//
+//namespace MainBundle\Entity;
+//
+//use Doctrine\ORM\Mapping as ORM;
+//
+///**
+// * User
+// *
+// * @ORM\Table(name="user")
+// * @ORM\Entity(repositoryClass="MainBundle\Repository\UserRepository")
+// */
+//class User
+//{
+//    /**
+//     * @var int
+//     *
+//     * @ORM\Column(name="id", type="integer")
+//     * @ORM\Id
+//     * @ORM\GeneratedValue(strategy="AUTO")
+//     */
+//    private $id;
+//
+//
+//    /**
+//     * Get id
+//     *
+//     * @return int
+//     */
+//    public function getId()
+//    {
+//        return $this->id;
+//    }
+//}
+//
