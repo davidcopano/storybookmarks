3b224c6316b4dbba1940d79b7be5ff57b31526c4
davidcopano <davidcopano96@gmail.com>
2018-06-13 18:06:18 +0200

    Quitado AppBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new MainBundle\MainBundle(),
         ];
 
