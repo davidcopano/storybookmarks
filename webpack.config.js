0e4f7aca791be66429770047790156d611242680
davidcopano <davidcopano96@gmail.com>
2018-06-15 16:19:38 +0200

    Añadido Encore (sass, un solo archivo JS)

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// the project directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    // will create web/build/app.js and web/build/app.scss
+    .addEntry('app', './assets/js/app.js')
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    // enable source maps during development
+    .enableSourceMaps(!Encore.isProduction())
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
+    // .enableBuildNotifications()
+
+// create hashed filenames (e.g. app.abc123.scss)
+// .enableVersioning()
+
+// allow sass/scss files to be processed
+    .enableSassLoader()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
