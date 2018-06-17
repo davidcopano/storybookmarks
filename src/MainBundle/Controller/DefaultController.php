561ca72ac4d09890e4f3295df376b9cec9b02643
davidcopano <davidcopano96@gmail.com>
2018-06-17 19:42:05 +0200

    Hecho cambiar idioma

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -5,13 +5,22 @@ namespace MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="root")
      */
-    public function indexAction(Request $request)
+    public function rootAction()
+    {
+        return $this->redirectToRoute('index');
+    }
+
+    /**
+     * @Route("{_locale}/", name="index")
+     */
+    public function indexAction()
     {
         return $this->render('@Main/Default/index.html.twig');
     }
