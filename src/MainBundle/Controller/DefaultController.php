7a544e4d151064c1cf5b4d523fb3df8825da35b3
davidcopano <davidcopano96@gmail.com>
2018-06-17 18:53:33 +0200

    Hecho desplegable idioma

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -4,13 +4,14 @@ namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         return $this->render('@Main/Default/index.html.twig');
     }
