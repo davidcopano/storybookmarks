a13dfae0f7bbd5340aed4f707c10f590afe62b72
davidcopano <davidcopano96@gmail.com>
2018-06-25 16:46:25 +0200

    Hecho enviar correo electronico basico

--- src/MainBundle/Controller/IndexController.php
+++ src/MainBundle/Controller/IndexController.php
@@ -2,11 +2,13 @@
 
 namespace MainBundle\Controller;
 
+use Psr\Log\LoggerInterface;
+use Swift_Mailer;
+use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 class IndexController extends Controller
 {
@@ -29,18 +31,32 @@ class IndexController extends Controller
     /**
      * @Route("{_locale}/send_form", name="index.send_form")
      * @param Request $request
-     * @param \Swift_Mailer $mailer
+     * @param Swift_Mailer $mailer
+     * @return JsonResponse
      */
-    public function sendFormAction(Request $request, \Swift_Mailer $mailer)
+    public function sendFormAction(Request $request, Swift_Mailer $mailer, LoggerInterface $logger)
     {
-        // var_dump($_POST);die;
+        $data = $request->request->all();
 
-        $message = (new \Swift_Message('Hola_EDIT'))
-                    ->setFrom('niidea@gmail.com')
+        if (isset($data['email']) && isset($data['subject']) && isset($data['description'])) {
+            try {
+                $message = (new Swift_Message('Contacto storybookmarks: ' . $data['subject']))
+                    ->setFrom('info@storybookmarks.com')
                     ->setTo('davidcopano96@gmail.com')
                     ->setBody('Hola desde correo en PHP/Symfony');
 
-        $numSent = $mailer->send($message);
-        printf("Sent %d messages\n", $numSent);die;
+                $mailer->send($message);
+
+                return new JsonResponse(['type' => 'ok']);
+            }
+            catch (\Exception $exception) {
+                $logger->error('[INDEX] Error al enviar formulario de contacto: ' . $exception->getMessage());
+                return new JsonResponse(['type' => 'error']);
+            }
+        }
+        else {
+            $logger->error('[INDEX] No se han proporcionado suficientes datos para enviar el form. de contacto');
+            return new JsonResponse(['type' => 'error']);
+        }
     }
 }
