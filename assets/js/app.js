9667a79e7a38bfc1cdd15c78dbc07781e6943234
davidcopano <davidcopano96@gmail.com>
2018-07-08 18:47:20 +0200

    Añadidos scroll suave

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,39 @@ require('animate.css');
 var $ = require('jquery');
 require('bootstrap');
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('[data-toggle="popover"]').popover();
 });
+
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+
+
+function elmYPosition(eID) {
+    let elm = document.getElementById(eID);
+    let y = elm.offsetTop;
+    let node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    }
+    return y;
+}
+
+
+function smoothScroll(id) {
+    $('html, body').animate({ scrollTop: $('#' + id).offset().top }, 'slow');
+    return false
+}
+
+window.currentYPosition = currentYPosition;
+window.elmYPosition = elmYPosition;
+window.smoothScroll = smoothScroll;
