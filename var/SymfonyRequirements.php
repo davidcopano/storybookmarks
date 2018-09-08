d783902cf5556f7c1addbb6aaabd4048bbf716c1
davidcopano <davidcopano96@gmail.com>
2018-09-08 16:12:03 +0200

    Muestra fecha

--- var/SymfonyRequirements.php
+++ var/SymfonyRequirements.php
@@ -5,7 +5,7 @@
  *
  * (c) Fabien Potencier <fabien@symfony.com>
  *
- * For the full copyright and license information, please view the LICENSE.md
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
