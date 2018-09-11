21d61123217991aec5c587fb96da30c0e84a5bea
davidcopano <davidcopano96@gmail.com>
2018-09-11 17:02:06 +0200

    Haciendo registro de conexión

--- src/MainBundle/Listeners/LoginListener.php
+++ src/MainBundle/Listeners/LoginListener.php
@@ -2,21 +2,31 @@
 
 namespace MainBundle\Listeners;
 
-use FOS\UserBundle\Model\UserManagerInterface;
+use Doctrine\ORM\EntityManager;
+use MainBundle\Entity\UserConnection;
+use MainBundle\Utilities\UserInfo;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class LoginListener {
 
-    protected $userManager;
+    protected $entityManager;
 
-    public function __construct(UserManagerInterface $userManager)
+    public function __construct(EntityManager $entityManager)
     {
-        $this->userManager = $userManager;
+        $this->entityManager = $entityManager;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
     {
         $user = $event->getAuthenticationToken()->getUser();
-
+        $userConnection = new UserConnection();
+        $userConnection->setUser($user)
+                    ->setIp(UserInfo::getIp())
+                    ->setBrowser(UserInfo::getBrowser())
+                    ->setDevice(UserInfo::getDevice())
+                    ->setOperatingSystem(UserInfo::getOperatingSystem())
+                    ->setUserAgent(UserInfo::getUserAgent());
+        $this->entityManager->persist($userConnection);
+        $this->entityManager->flush();
     }
 }
