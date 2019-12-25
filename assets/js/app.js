279f38cab3e4c7a92e13878e728afe775b12c4dd
David Copano <davidcopano96@gmail.com>
2019-12-25 17:44:56 +0100

    [Tema oscuro] Añadir cookie para modo oscuro con JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,9 +59,47 @@ function copyToClipboard (str) {
     document.body.removeChild(el);
 }
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+
 window.currentYPosition = currentYPosition;
 window.elmYPosition = elmYPosition;
 window.smoothScroll = smoothScroll;
 window.Cookies = Cookies;
 window.copyToClipboard = copyToClipboard;
 window.$ = $;
+window.setCookie = setCookie;
+window.getCookie = getCookie;
+window.checkCookie = checkCookie;
